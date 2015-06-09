629deea5dc16ed88fd62afe174f38e36bbe9e334
faceach <faceach@gmail.com>
2015-06-09 18:48:40 +0800

    android package name

--- src/android/OxfordSpeechRecognition.java
+++ src/android/OxfordSpeechRecognition.java
@@ -17,7 +17,7 @@ FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TOR
 ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-package com.microsoft.cordova.speechrecognition;
+package com.projectoxford.cordova.speechrecognition;
 
 import java.util.ArrayList;
 
